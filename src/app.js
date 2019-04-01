fde6299c21f04fd92159f68a9ee7d8a2541e9ec2
Rafael Jiménez Cabrera <33703980+Raejimenezca@users.noreply.github.com>
2019-03-31 21:29:51 -0500

    CRUD de 'cursos' y 'estudiantes' implementado completamente

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
+require('./helpers');
 const path = require('path');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
 const crud = require('./crud');
-//require('./helpers');
-
+const crudCursos = require('./crudCurso');
 const directorioPublico = path.join(__dirname, '../public');
 const directorioPartials = path.join(__dirname, '../partials');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(directorioPublico));
 hbs.registerPartials(directorioPartials);
@@ -27,8 +28,31 @@ app.get('/ingreso', (req, res) => {
     res.render('ingreso');
 });
 
+app.get('/crearCursos', (req, res) => {
+    res.render('crearCursos');
+});
+
+app.get('/eliminar_Est', (req, res) => {
+    if (req.query.id != undefined) {
+        crud.eliminar(req.query.id);
+    }
+    res.render('eliminar_est');
+});
+
+app.get('/actualizar_Est', (req, res) => {
+    res.render('actualizar_est');
+});
+
+app.get('/listar_estudiantes', (req, res) => {
+    //console.log(req.query.id);
+    if (req.query.id != undefined) {
+        crud.actualizar(req.query.id, req.query.nombre, req.query.correo, req.query.telefono, req.query.tipo);
+    }
+    res.render('listar_estudiantes');
+});
+
 app.get('/administracion', (req, res) => {
-    console.log(req.query);
+    //console.log(req.query);
     crud.listar();
     let estudiante = listaEstudiantes.find(busqueda => busqueda.id == req.query.id);
     if (!estudiante) {
@@ -54,7 +78,7 @@ app.get('/administracion', (req, res) => {
 app.get('/cursos_disponibles', (req, res) => {
     crud.listar();
     let estudiante = listaEstudiantes.find(buscar => buscar.id == req.query.id);
-    
+    let cursos = listaCurso.find(busqueda => busqueda.idcurso == req.query.idcurso);
     if (!estudiante) {
         crud.crear(req.query);
         res.render('cursos_disponibles', {
@@ -63,6 +87,16 @@ app.get('/cursos_disponibles', (req, res) => {
             correo: req.query.correo,
             telefono: req.query.telefono
         });
+    } else if (!cursos) {
+        crudCursos.crear(req.query);
+        res.render('cursos_disponibles', {
+            idcurso: req.query.idcurso,
+            nombrecurso: req.query.nombrecurso,
+            valor: req.query.valor,
+            descripcioncurso: req.query.descripcioncurso,
+            modalidad: req.query.modalidad,
+            intensidadhoraria: req.query.intensidadhoraria
+        });
     }
     else {
         console.log('Registo invalido');
@@ -70,10 +104,10 @@ app.get('/cursos_disponibles', (req, res) => {
     }
 });
 
-app.get('*', (req, res) => {
+/*app.get('*', (req, res) => {
     res.render('error');
-});
+});*/
 
-app.listen(3000, () => {
-    console.log("Express server on port 3000");
+app.listen(port, () => {
+    console.log("Escuchando en el puerto " + port);
 });
